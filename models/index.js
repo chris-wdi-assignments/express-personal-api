b6ba0f7c85a8ab512358dde1e17ec829121f593c
Christopher Fujino <christopherfujino@gmail.com>
2017-07-19 14:39:33 -0700

    fixed: mongoose deprecation errors

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", { useMongoClient: true });
+mongoose.Promise = global.Promise;  // fix deprecation err
 
 module.exports = {
   Record: require('./Record.js')
