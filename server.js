6af697cab73c0def9bdb41981d1a5fca33cc9971
Christopher Fujino <christopherfujino@gmail.com>
2017-07-19 10:11:29 -0700

    added: hardcoded data

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
  * DATABASE *
  ************/
 
-// var db = require('./models');
+var db = require('./models');
 
 /**********
  * ROUTES *
@@ -41,23 +41,15 @@ app.get('/', function homepage(req, res) {
 /*
  * JSON API Endpoints
  */
+const apiDocumentation = require('./docs/api.json');  // require at runtime so
+                          // it's already in memory by the time someone GETs
 
 app.get('/api', function apiIndex(req, res) {
-  // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
-  // It would be seriously overkill to save any of this to your database.
-  // But you should change almost every line of this response.
-  res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
-    endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
-    ]
-  })
+  res.json(apiDocumentation);
 });
+
+const profileJson = require('./docs/profile.json'); // require at runtime
+app.get('/api/profile', (req, res) => res.json(profileJson));
 
 /**********
  * SERVER *
